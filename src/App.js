5d0686e48f73d97f032e0ed07a33032f8f376e08
Greq <greqorian@gmail.com>
2022-06-28 16:43:30 +0200

    feat: initialise app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const restaurants = {
+    mcdonald: {
+      meals: {
+        meal1: {
+          ingredients: {
+            one: 100,
+            two: 200,
+            three: 300,
+          },
+        },
+        meal2: {
+          ingredients: {
+            one: 100,
+            two: 200,
+            three: 300,
+          },
+        },
+        meal3: {
+          ingredients: {
+            one: 100,
+            two: 200,
+            three: 300,
+          },
+        },
+      },
+    },
+    kfc: {},
+    subway: {},
+  };
+
+  const [selectedRes, setSelectedRes] = useState(undefined);
+  const [selectedMeal, setSelectedMeal] = useState(undefined);
+
+  console.log("selectedRes", selectedRes);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Restaurans:</div>
+      {Object.keys(restaurants).map((key, index) => {
+        return (
+          <button
+            onClick={() => {
+              setSelectedRes(key);
+            }}
+          >
+            {key}
+          </button>
+        );
+      })}
+
+      <hr />
+      <div>Meals:</div>
+      {selectedRes &&
+        Object.keys(restaurants[selectedRes].meals).map((key, index) => {
+          return (
+            <button
+              onClick={() => {
+                setSelectedMeal(key);
+              }}
+            >
+              {key}
+            </button>
+          );
+        })}
+
+      <hr />
+      <div>Igredients:</div>
+      {selectedMeal &&
+        Object.keys(
+          restaurants[selectedRes].meals[selectedMeal].ingredients
+        ).map((key, index) => {
+          return (
+            <li>
+              {key}:{" "}
+              {restaurants[selectedRes].meals[selectedMeal].ingredients[key]}
+            </li>
+          );
+        })}
     </div>
   );
 }
